963e08361b0833330d4699e359a9792d31efed03
mvaldetaro <mvaldetaro@gmail.com>
2020-02-05 08:51:57 -0200

    Implementa rotas de crud

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express');
 const server = express();
 
-// Query params = ?teste=1
-// Route params =/users/1
-// Request body = {"name": "Magno", "email": "magno@mail.com"} //payload
+server.use(express.json());
+
+// Query params = ?teste=1 (req.query)
+// Route params =/users/1   (req.params)
+// Request body = {"name": "Magno", "email": "magno@mail.com"} (req.body)
 
 const users = ['Magno', 'Bruno', 'Robson', 'Camile', 'Ávila'];
+
+server.get('/users', (req, res) => {
+  return res.json(users);
+});
 
 server.get('/users/:index', (req, res) => {
   const { index } = req.params;
   return res.json(users[index]);
+});
+
+server.post('/users/', (req, res) => {
+  const { name } = req.body;
+  users.push(name);
+  return res.json(users);
+});
+
+server.put('/users/:index', (req, res) => {
+  const { index } = req.params;
+  const { name } = req.body;
+
+  users[index] = name;
+
+  return res.json(users);
+});
+
+server.delete('/users/:index', (req, res) => {
+  const { index } = req.params;
+
+  users.splice(index, 1);
+
+  return res.send();
 });
 
 server.listen(3000);

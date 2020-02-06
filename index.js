ea385103bc41aab145adb5d0edf7ae45f6eb1c88
mvaldetaro <mvaldetaro@gmail.com>
2020-02-06 08:11:38 -0200

    Middleware global

--- index.js
+++ index.js
@@ -9,6 +9,14 @@ server.use(express.json());
 
 const users = ['Magno', 'Bruno', 'Robson', 'Camile', 'Ávila'];
 
+// Middleware global
+server.use((req, res, next) => {
+  console.time('Request');
+  console.log(`Método: ${req.method}; URL: ${req.url};`);
+  next();
+  console.timeEnd('Request');
+});
+
 server.get('/users', (req, res) => {
   return res.json(users);
 });
